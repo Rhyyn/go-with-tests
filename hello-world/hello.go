aa085b851c598e4a159ef3973b7dcbab1ce12579
Rhyn <tonymigeonpro@gmail.com>
2024-03-02 15:37:39 +0100

    Small refactor + formatting

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,17 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+const (
+	french             = "French"
+	spanish            = "Spanish"
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPreifx(language) + name
+}
+
+func greetingPreifx(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello(("world")))
+	fmt.Println(Hello("world", ""))
 }
